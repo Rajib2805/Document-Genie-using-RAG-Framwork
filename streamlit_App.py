2fddd511744df43c7c795d91db909c4d24a6e796
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2024-08-17 00:03:00 +0530

    Update streamlit_App.py

--- streamlit_App.py
+++ streamlit_App.py
@@ -1,16 +1,14 @@
 import streamlit as st
+import faiss
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import DirectoryLoader
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
-from langchain_community.vectorstores import FAISS
+from langchain_community.vectorstores.faiss import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
-import os
-
-import faiss
 from langchain_community.docstore.in_memory import InMemoryDocstore
 
 st.set_page_config(page_title="Document Genie", layout="wide")
